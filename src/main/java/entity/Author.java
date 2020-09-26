f5711705b09d9f807b43087cda9e56fe7408a7e4
Madara <madara_kruzina@inbox.lv>
2020-09-26 14:43:34 +1200

    Author

--- src/main/java/entity/Author.java
+++ src/main/java/entity/Author.java
@@ -5,6 +5,52 @@ import javax.persistence.*;
 import java.util.Set;
 
 @Entity
+@Table(name = "author")
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "authorId")
+    private Integer id;
+
+    @Column(name = "firstName", length = 25)
+    private String firstName;
+
+    @Column(name = "lastName", length = 25)
+    private String lastName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 
 }
