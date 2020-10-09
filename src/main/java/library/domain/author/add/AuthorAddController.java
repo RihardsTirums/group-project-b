113a46599a9cf0a13b0df3a416086fc89908607d
Madara <madara_kruzina@inbox.lv>
2020-10-10 11:04:30 +1200

    AuthorAddController - update

--- src/main/java/library/domain/author/add/AuthorAddController.java
+++ src/main/java/library/domain/author/add/AuthorAddController.java
@@ -14,57 +14,66 @@ import library.domain.book.BookRepository;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-    public class AuthorAddController implements Initializable {
+public class AuthorAddController implements Initializable {
 
-        private final AuthorRepository authorRepository = new AuthorRepository();
+    private final AuthorRepository authorRepository = new AuthorRepository();
 
-        @FXML private JFXTextField firstName;
-        @FXML private JFXTextField lastName;
+    @FXML
+    private JFXTextField firstName;
+    @FXML
+    private JFXTextField lastName;
 
-        @FXML private StackPane rootPane;
-
-
-
-        private Author editable;
-
-        @Override
-        public void initialize(URL url, ResourceBundle rb) {
-        }
-        public void setEditable(Author author) {
-            this.editable = author;
-        }
-
-        @FXML
-        private void addAuthor(ActionEvent event) {
-            String authorFirstName = firstName.getText();
-            String authorLastName = lastName.getText();
+    @FXML
+    private StackPane rootPane;
 
 
-            if (authorFirstName.isEmpty() || authorLastName.isEmpty()) {
-                //TODO show user alert that all fields have to be filled
-                return;
-            }
+    private Author editable;
 
-            clearEntries();
-            closeStage();
-        }
-
-        @FXML
-        private void cancel(ActionEvent event) {
-            closeStage();
-        }
-
-        private void clearEntries() {
-            editable = null;
-            firstName.clear();
-            lastName.clear();
-
-        }
-
-        private void closeStage() {
-            Stage stage = (Stage) rootPane.getScene().getWindow();
-            stage.close();
-        }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
     }
 
+    public void setEditable(Author author) {
+        this.editable = author;
+    }
+
+    @FXML
+    private void addAuthor(ActionEvent event) {
+        String authorFirstName = firstName.getText();
+        String authorLastName = lastName.getText();
+
+
+        if (authorFirstName.isEmpty() || authorLastName.isEmpty()) {
+            //TODO show user alert that all fields have to be filled
+            return;
+        }
+        if (editable == null) {
+            authorRepository.save(new Author(authorFirstName, authorLastName));
+        } else {
+            Author author = authorRepository.findOne(editable.getId());
+            author.setFirstName(authorFirstName);
+            author.setLastName(authorLastName);
+        }
+
+        clearEntries();
+        closeStage();
+    }
+
+    @FXML
+    private void cancel(ActionEvent event) {
+        closeStage();
+    }
+
+    private void clearEntries() {
+        editable = null;
+        firstName.clear();
+        lastName.clear();
+
+    }
+
+    private void closeStage() {
+        Stage stage = (Stage) rootPane.getScene().getWindow();
+        stage.close();
+    }
 }
+
